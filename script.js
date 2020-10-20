096610934a2e3ecc3159a641514c248d099aa1ef
Letícia <leticia.rossaneis@gmail.com>
2020-10-20 16:57:04 -0300

    Cobrinha, comida, condição de fim de jogo

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 //Inicia o canvas
 function criarBG() {
@@ -28,9 +30,41 @@ function criarCobrinha() {
     }
 }
 
+//Comida
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+//Evento para movimentação
+document.addEventListener('keydown', update);
+
+//Movimentação pelas teclas
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo() {
+    //Voltar ao começo da tela e impedir que a cobrinha suma
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    //Condição de fim de jogo
+    for(i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert("Game Over =(");
+        }
+    }
+
     criarBG();
     criarCobrinha();
+    drawFood();
 
     //Posição inicial
     let snakeX = snake[0].x;
@@ -39,12 +73,25 @@ function iniciarJogo() {
     //Coordenadas
     if (direction == "right") snakeX += box;
     if (direction == "left") snakeX -= box;
-
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    //Retirar o último elemento para a cobrinha 'andar'
-    snake.pop();
+    //Movimento e crescimento
+    if (snakeX != food.x || snakeY != food.y) {
+        //Retirar o último elemento para a cobrinha 'andar'
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    //Inserir novo elemento para 'andar'
+    snake.unshift(newHead);
 }
 
 //Atualizar o jogo de tempos em tempos até o jogo acabar
